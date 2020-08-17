c22057509003db5a7bb84257e47f95d15eaac621
WongJian1990 <wangjianking1990@163.com>
2020-08-17 10:39:03 +0800

    redis API练习

--- redis/main.go
+++ redis/main.go
@@ -56,6 +56,7 @@ func main() {
 		fmt.Println("Get redis connection failed: ", err)
 		return
 	}
+	//设置string
 	_, err = conn.Do("set", "gemini/vdts", "hello world")
 	if err != nil {
 		fmt.Println("ERR: ", err)
@@ -63,15 +64,124 @@ func main() {
 	conn.Close()
 
 	conn, err = Get(pools, "gemini/zdts")
+	defer conn.Close()
 	if err != nil {
 		fmt.Println("Get redis connection failed: ", err)
 		return
 	}
 	_, err = conn.Do("set", "gemini/zdts", "hello world")
 	if err != nil {
-		fmt.Println("ERR: ", err)
+		fmt.Println("ERR[SET]: ", err)
+	}
+	//获取string
+	reply, err := redis.String(conn.Do("get", "gemini/zdts"))
+	if err != nil {
+		fmt.Println("ERR[GET]: ", err)
+	}
+	fmt.Println("reply: ", reply)
+
+	//设置list
+	_, err = conn.Do("lpush", "gemini/zdts/list", "hello", "world", "from", "wang", "jian")
+
+	//获取list
+	replies, err := redis.Values(conn.Do("lrange", "gemini/zdts/list", 0, 20))
+	if err != nil {
+		fmt.Println("ERR[LRANGE]: ", err)
+	}
+	for _, v := range replies {
+		// fmt.Println("kind: ", reflect.TypeOf(v).Kind().String())
+		fmt.Println("value: ", string(v.([]byte)))
 	}
 
-	conn.Close()
+	//删除list
+	_, err = conn.Do("del", "gemini/zdts/list")
+	if err != nil {
+
+		fmt.Println("ERR[DEL]: ", err)
+	}
+
+	_, err = conn.Do("lpush", "gemini/zdts/list", 10, 20, 30, 40, 50)
+	replies, err = redis.Values(conn.Do("lrange", "gemini/zdts/list", 0, 20))
+	if err != nil {
+		fmt.Println("ERR[LRANGE]: ", err)
+	}
+	for _, v := range replies {
+		// fmt.Println("kind: ", reflect.TypeOf(v).Kind().String())
+		fmt.Println("value: ", string(v.([]byte)))
+	}
+
+	//删除list
+	_, err = conn.Do("del", "gemini/zdts/list")
+	if err != nil {
+
+		fmt.Println("ERR[DEL]: ", err)
+	}
+
+	//设置 hash字典
+	_, err = conn.Do("hmset", "gemini/zdts/hash", "value1", 10, "value2", 20, "value3", 30, "value3", 40)
+	if err != nil {
+		fmt.Println("ERR[HMSET]: ", err)
+	}
+	//获取hash字典
+	replies, err = redis.Values(conn.Do("HGETALL", "gemini/zdts/hash"))
+	if err != nil {
+		fmt.Println("ERR[HGETALL]: ", err)
+	}
+	for _, v := range replies {
+		fmt.Printf("v:%v\n", string(v.([]byte)))
+	}
+
+	//删除
+	_, err = conn.Do("del", "gemini/zdts/hash")
+	if err != nil {
+		fmt.Println("ERR[DEL]: ", err)
+	}
+
+	//设置set集合
+	_, err = conn.Do("sadd", "gemini/zdts/set", "value1", "value2", "value2", "value3", "value3", "value4", "value4")
+	if err != nil {
+		fmt.Println("ERR[SADD]: ", err)
+	}
+	//获取所有成员
+	replies, err = redis.Values(conn.Do("smembers", "gemini/zdts/set"))
+	if err != nil {
+		fmt.Println("ERR[SMEMBERS]: ", err)
+	}
+	for _, v := range replies {
+		fmt.Println("value: ", string(v.([]byte)))
+	}
+
+	//删除set key
+	_, err = conn.Do("del", "gemini/zdts/set")
+	if err != nil {
+		fmt.Println("ERR[DEL]: ", err)
+	}
+
+	t1 := time.Now().UnixNano()
+	time.Sleep(1 * time.Millisecond)
+	t2 := time.Now().UnixNano()
+	time.Sleep(1 * time.Millisecond)
+	t3 := time.Now().UnixNano()
+
+	//设置有序集合
+	_, err = conn.Do("zadd", "gemini/zdts/zset", t3, "value1", t2, "value2",
+		t1, "value3")
+	if err != nil {
+		fmt.Println("ERR[ZADD]: ", err)
+	}
+	//获取range范围内的成员
+	replies, err = redis.Values(conn.Do("zrange", "gemini/zdts/zset", 0, 20, "WITHSCORES"))
+	if err != nil {
+		fmt.Println("ERR[ZRANGE]: ", err)
+	}
+	for _, v := range replies {
+		fmt.Println("value: ", string(v.([]byte)))
+	}
+
+	//删除 zset key
+	_, err = conn.Do("del", "gemini/zdts/zset")
+	if err != nil {
+		fmt.Println("ERR[DEL]: ", err)
+	}
 
 }
