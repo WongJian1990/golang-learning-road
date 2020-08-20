204bc5a3eaddb3330d7a2fdc4122a298061cf44b
WongJian1990 <wangjianking1990@163.com>
2020-08-20 08:46:23 +0800

    bufio API练习

--- basic/bufferio/main.go
+++ basic/bufferio/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -71,7 +72,7 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	defer file.Close()
+
 	w := bufio.NewWriter(file)
 	_, err = w.Write([]byte(string("Test Write API 1\n")))
 	if err != nil {
@@ -88,4 +89,53 @@ func main() {
 		log.Fatalln(err)
 	}
 	w.Flush()
+	_, err = w.WriteString("Test Write API 6 \n")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	w.Flush()
+
+	file.Close()
+	//Scanner
+	file, err = os.OpenFile("readme.txt", os.O_RDONLY, 0666)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("reading standard input: %v", err)
+	}
+
+	//custom
+	// const input = "1234 5678 123456789876543210"
+	// scanner = bufio.NewScanner(strings.NewReader(input))
+	// split := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	// 	advance, token, err = bufio.ScanWords(data, atEOF)
+	// 	if err == nil && token != nil {
+	// 		_, err = strconv.ParseInt(string(token), 10, 32)
+	// 	}
+	// 	return
+	// }
+	// scanner.Split(split)
+	// for scanner.Scan() {
+	// 	fmt.Println(scanner.Text())
+	// }
+	// if err := scanner.Err(); err != nil {
+	// 	log.Fatalf("Invalid input: %v", err)
+	// }
+
+	//scanner word
+
+	const input2 = "2020/08/19 16:54:36 Invalid input: strconv.ParseInt: parsing \"123456789876543210\": value out of range"
+	scanner = bufio.NewScanner(strings.NewReader(input2))
+	scanner.Split(bufio.ScanWords)
+	count = 0
+	for scanner.Scan() {
+		count++
+		fmt.Println("Text: ", scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Invalid input:%v", err)
+	}
+	fmt.Println("count: ", count)
 }
--- basic/bufferio/readme.txt
+++ basic/bufferio/readme.txt
@@ -1,3 +1,4 @@
 Test Write API 1
 Test Write API 2
 Test Write API 5
+Test Write API 6 
